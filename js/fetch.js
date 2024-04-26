3c115540a0bf820e93846c1a822cfd6879bab680
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-04-26 15:47:05 +0700

    the trid push

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,7 @@ import { get } from "https://bukulapak.github.io/api/process.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabel } from "./table.js";
-let urlAPI = "https://ws-serli2024-48bfd02e8f8c.herokuapp.com/";
+let urlAPI = "https://ws-serli2024-48bfd02e8f8c.herokuapp.com/presensi";
 get(urlAPI, isiTablePresensi);
 function isiTablePresensi(results) {
     results.forEach(isiRow);
