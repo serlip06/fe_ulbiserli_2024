155d03242c45d06eadfc2571e15d3290d6bc9c74
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-04-26 15:40:09 +0700

    push2

--- js/table.js
+++ js/table.js
@@ -1,4 +1,4 @@
-export let isiTable = `
+export let isiTabel = `
 <tr class="h-18 border-b border-coolGray-100">
 <th class="whitespace-nowrap px-4 bg-white text-left">
   <div class="flex items-center -m-2">
