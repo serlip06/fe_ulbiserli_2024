5fd8b6d603f6285d309fa273a16cc8643cd455a1
serlip06 <114634943+serlip06@users.noreply.github.com>
2024-06-07 19:14:25 +0700

    updatee!!thelast_maybe??

---
+++ js/controller/delete.js
@@ -0,0 +1,17 @@
+function deleteData(IDHAPUS) {
+    var presensiId = IDHAPUS;
+    var target_url = "https://ws-serli2024-48bfd02e8f8c.herokuapp.com/delete/" + presensiId;
+
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            alert(result.message);
+            location.reload();
+        })
+        .catch(error => console.log('Error:', error));
+}
